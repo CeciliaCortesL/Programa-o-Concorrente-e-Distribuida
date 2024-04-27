08c9b9b9b21beabeed9503e98bce3027555c8b85
Cecilia Cortes Lopes <ceciliacorteslopes@gmail.com>
2024-04-27 12:47:17 -0300

    loja

--- Atividade Pratica Individual 1/src/Loja.java
+++ Atividade Pratica Individual 1/src/Loja.java
@@ -1,3 +1,19 @@
 public class Loja {
-    
+    private final Conta conta;
+    private final double salarioFuncionario = 1400;
+
+    public Loja(double saldoInicial) {
+        this.conta = new Conta(saldoInicial);
+    }
+
+    public void pagarFuncionario() {
+        if (conta.getSaldo() >= salarioFuncionario) {
+            conta.debitar(salarioFuncionario);
+            System.out.println("Funcionário pago: R$ " + salarioFuncionario);
+        }
+    }
+
+    public void receberPagamento(double valor) {
+        conta.creditar(valor);
+    }    
 }
