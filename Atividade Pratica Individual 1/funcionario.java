7fb8360206c35bdfc32bb060ed9806c5c8059a54
Cecilia Cortes Lopes <ceciliacorteslopes@gmail.com>
2024-04-26 22:30:38 -0300

    estrutura funcionario

--- Atividade Pratica Individual 1/funcionario.java
+++ Atividade Pratica Individual 1/funcionario.java
@@ -1,3 +1,28 @@
-public class Funcionario {
+public class Funcionario extends Thread {
+    private final Loja loja;
+    private final Conta salarioConta;
+    private final Conta investimentoConta;
 
+    public Funcionario(Loja loja) {
+        this.loja = loja;
+        this.salarioConta = new Conta(0);
+        this.investimentoConta = new Conta(0);
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            loja.pagarFuncionario();
+            double salario = 1400;
+            salarioConta.creditar(salario);
+            double valorInvestimento = salario * 0.2;
+            salarioConta.debitar(valorInvestimento);
+            investimentoConta.creditar(valorInvestimento);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
