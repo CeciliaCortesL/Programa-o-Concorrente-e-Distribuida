8f799ec5c8bc2650606de2a2f7a29659973642a2
Cecilia Cortes Lopes <ceciliacorteslopes@gmail.com>
2024-04-26 22:35:03 -0300

    estrutura sistema bancario

--- Atividade Pratica Individual 1/sistemaBancario.java
+++ Atividade Pratica Individual 1/sistemaBancario.java
@@ -1,6 +1,25 @@
+public class SistemaBancario {
+    public static void main(String[] args) {
+        Conta[] contasClientes = new Conta[5];
+        for (int i = 0; i < contasClientes.length; i++) {
+            contasClientes[i] = new Conta(1000);
+        }
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+        Loja[] lojas = new Loja[2];
+        for (int i = 0; i < lojas.length; i++) {
+            lojas[i] = new Loja(0);
+        }
 
-public class sistemaBancario {
+        Funcionario[] funcionarios = new Funcionario[4];
+        for (int i = 0; i < funcionarios.length; i++) {
+            funcionarios[i] = new Funcionario(lojas[i / 2]);
+            funcionarios[i].start();
+        }
+
+        Cliente[] clientes = new Cliente[5];
+        for (int i = 0; i < clientes.length; i++) {
+            clientes[i] = new Cliente(contasClientes[i], lojas);
+            clientes[i].start();
+        }
+    }
 }
